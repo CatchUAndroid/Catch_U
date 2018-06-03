02351eed0f4977c5f38c5706ca38f60b5d723f62
ugurgog <32577192+ugurgog@users.noreply.github.com>
2018-06-03 12:11:38 +0300

    Fragments added

---
+++ app/src/main/java/uren/com/catchu/MainPackage/Interfaces/IOnBackPressed.java
@@ -0,0 +1,10 @@
+package uren.com.catchu.MainPackage.Interfaces;
+
+/**
+ * Created by mac on 20.01.2018.
+ */
+
+public interface IOnBackPressed {
+
+    boolean onBackPressed();
+}
---
+++ app/src/main/java/uren/com/catchu/MainPackage/MainFragments/AddPinFragment.java
@@ -0,0 +1,33 @@
+package uren.com.catchu.MainPackage.MainFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+import uren.com.catchu.MainPackage.MainActivity;
+import uren.com.catchu.R;
+
+public class AddPinFragment extends BaseFragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_add_pin, container, false);
+
+        ButterKnife.bind(this, view);
+
+        ( (MainActivity)getActivity()).updateToolbarTitle("Add Pin");
+
+
+        return view;
+    }
+
+
+}
